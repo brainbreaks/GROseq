8b2a1cea4cd499c87b72fdb4bac8d3c6bfdc8a5c
Sergej Andrejev <sandrejev@gmail.com>
2020-09-16 14:20:22 +0200

    Fix paths in download script Add homer main package to download

--- preprocess/download.py
+++ preprocess/download.py
@@ -10,9 +10,10 @@ import shutil
 import os
 import glob
 
+
 def download_file(url, dest=None, overwrite=False):
 	if not overwrite and dest and os.path.isfile(dest):
-		print('Downloading file "{}". Already exists, skipping...'.format(dest))
+		print('Downloading file "{}" ==> "{}". Already exists, skipping...'.format(url, dest))
 		return dest
 
 	if re.match("^(http|ftp)", url):
@@ -23,7 +24,7 @@ def download_file(url, dest=None, overwrite=False):
 		progress_bar = tqdm.tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)
 		with tempfile.NamedTemporaryFile(mode="wb", delete=False) as file:
 			path = file.name
-			progress_bar.set_description('Downloading file "{}"...'.format(dest))
+			progress_bar.set_description('Downloading file "{}" ==> "{}"...'.format(url, dest if dest else path))
 			for data in response.iter_content(block_size):
 				progress_bar.update(len(data))
 				file.write(data)
@@ -46,10 +47,10 @@ def download_file(url, dest=None, overwrite=False):
 
 def download_raw_genome(url, dest, overwrite=False):
 	if not overwrite and dest and os.path.isfile(dest):
-		print('Downloading bowtie index "{}". Already exists, skipping...'.format(dest))
+		print('Downloading raw genome "{}". Already exists, skipping...'.format(dest))
 		return
 
-	print('Downloading bowtie index "{}"\n'.format(url))
+	print('Downloading raw genome "{}"\n'.format(url))
 	path = download_file(url)
 
 	dest_chromFa = "{}_{}".format(dest, "chromFa")
@@ -72,7 +73,11 @@ def download_raw_genome(url, dest, overwrite=False):
 	shutil.rmtree(dest_chromFa)
 
 
-def download_bowtie2_index(url, dest):
+def download_bowtie2_index(url, dest, overwrite=False):
+	if not overwrite and dest and os.path.isfile(dest):
+		print('Downloading bowtie2 index "{}". Already exists, skipping...'.format(dest))
+		return
+
 	print("Downloading bowtie index '{}'\n".format(url))
 	path = download_file(url)
 
@@ -87,30 +92,29 @@ def download_bowtie2_index(url, dest):
 
 def main():
 	# This works
-	# download_bowtie2_index("https://genome-idx.s3.amazonaws.com/bt/mm9.zip", "data/mm9")
-	# download_bowtie2_index("https://genome-idx.s3.amazonaws.com/bt/hg19.zip", "data/hg19")
+	download_bowtie2_index("https://genome-idx.s3.amazonaws.com/bt/mm9.zip", "data/mm9")
+	download_bowtie2_index("https://genome-idx.s3.amazonaws.com/bt/hg19.zip", "data/hg19")
 
-	# download_raw_genome("http://hgdownload.cse.ucsc.edu/goldenPath/mm9/bigZips/chromFa.tar.gz", "data/mm9/mm9.fa")
-	# download_file("http://hgdownload.cse.ucsc.edu/goldenPath/mm9/bigZips/mm9.chrom.sizes", "data/mm9/mm9.chrom.sizes")
-	# download_raw_genome("http://hgdownload.cse.ucsc.edu/goldenPath/hg19/bigZips/chromFa.tar.gz", "data/hg19/hg19.fa")
-	# download_file("http://hgdownload.cse.ucsc.edu/goldenPath/hg19/bigZips/hg19.chrom.sizes", "data/hg19/hg19.chrom.sizes")
-	# download_file("http://homer.ucsd.edu/homer/data/genomes/hg19.v6.4.zip", "data/homer/hg19.v6.4.zip")
-	# download_file("http://homer.ucsd.edu/homer/data/genomes/mm9.v6.4.zip", "data/homer/mm9.v6.4.zip")
-	# download_file("http://homer.ucsd.edu/homer/data/organisms/human.v6.3.zip", "data/homer/human.v6.3.zip")
-	# download_file("http://homer.ucsd.edu/homer/data/organisms/mouse.v6.3.zip", "data/homer/mouse.v6.3.zip")
+	download_raw_genome("http://hgdownload.cse.ucsc.edu/goldenPath/mm9/bigZips/chromFa.tar.gz", "data/mm9/mm9.fa")
+	download_file("http://hgdownload.cse.ucsc.edu/goldenPath/mm9/bigZips/mm9.chrom.sizes", "data/mm9/mm9.chrom.sizes")
+	download_raw_genome("http://hgdownload.cse.ucsc.edu/goldenPath/hg19/bigZips/chromFa.tar.gz", "data/hg19/hg19.fa")
+	download_file("http://hgdownload.cse.ucsc.edu/goldenPath/hg19/bigZips/hg19.chrom.sizes", "data/hg19/hg19.chrom.sizes")
 
-	# download_file("https://github.com/BenLangmead/bowtie2/releases/download/v2.2.9/bowtie2-2.2.9-linux-x86_64.zip", "data/dependencies/bowtie2-2.2.9-linux-x86_64.zip")
-	# download_file("https://github.com/samtools/samtools/releases/download/1.6/samtools-1.6.tar.bz2", "data/dependencies/samtools-1.6.tar.bz2")
-	# download_file("https://github.com/arq5x/bedtools2/releases/download/v2.29.1/bedtools-2.29.1.tar.gz", "data/dependencies/bedtools-2.29.1.tar.gz")
-	# download_file("https://anaconda.org/bioconda/gfold/1.1.4/download/linux-64/gfold-1.1.4-gsl2.2_2.tar.bz2", "data/dependencies/gfold-1.1.4-gsl2.2_2.tar.bz2")
-	# download_file("http://www.artfiles.org/gnu.org/gsl/gsl-2.2.1.tar.gz", "data/dependencies/gsl-2.2.1.tar.gz")
-	# download_file("http://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64/wigToBigWig", "data/dependencies/wigToBigWig")
-	# download_file("http://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64/bigWigToWig", "data/dependencies/bigWigToWig")
-	#
-	#
-	# Test download raw genome
-	# download_raw_genome("/home/s215v/Workspace/GROseq/data/mm9/mm9_chromFa.tar.gz", "data/mm9/mm9.fa")
-	#
+	# Homer packages
+	download_file("http://homer.ucsd.edu/homer/data/software/homer.v4.11.1.zip", "data/dependencies/homer/homer.v4.11.1.zip")
+	download_file("http://homer.ucsd.edu/homer/data/genomes/hg19.v6.4.zip", "data/dependencies/homer/hg19.v6.4.zip")
+	download_file("http://homer.ucsd.edu/homer/data/genomes/mm9.v6.4.zip", "data/dependencies/homer/mm9.v6.4.zip")
+	download_file("http://homer.ucsd.edu/homer/data/organisms/human.v6.3.zip", "data/dependencies/homer/human.v6.3.zip")
+	download_file("http://homer.ucsd.edu/homer/data/organisms/mouse.v6.3.zip", "data/dependencies/homer/mouse.v6.3.zip")
+
+	# Download libraries used in the pipeline
+	download_file("https://github.com/BenLangmead/bowtie2/releases/download/v2.2.9/bowtie2-2.2.9-linux-x86_64.zip", "data/dependencies/bowtie2-2.2.9-linux-x86_64.zip")
+	download_file("https://github.com/samtools/samtools/releases/download/1.6/samtools-1.6.tar.bz2", "data/dependencies/samtools-1.6.tar.bz2")
+	download_file("https://github.com/arq5x/bedtools2/releases/download/v2.29.1/bedtools-2.29.1.tar.gz", "data/dependencies/bedtools-2.29.1.tar.gz")
+	download_file("https://anaconda.org/bioconda/gfold/1.1.4/download/linux-64/gfold-1.1.4-gsl2.2_2.tar.bz2", "data/dependencies/gfold-1.1.4-gsl2.2_2.tar.bz2")
+	download_file("http://www.artfiles.org/gnu.org/gsl/gsl-2.2.1.tar.gz", "data/dependencies/gsl-2.2.1.tar.gz")
+	download_file("http://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64/wigToBigWig", "data/dependencies/wigToBigWig")
+	download_file("http://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64/bigWigToWig", "data/dependencies/bigWigToWig")
 	download_file("https://github.com/bxlab/bx-python/archive/master.zip", "data/dependencies/bx-python.zip")
 
 #
